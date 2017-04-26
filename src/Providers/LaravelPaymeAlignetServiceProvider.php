06a3df57611f97537d0d1a1525691388e800dc0b
Helmer <helmer290192@hotmail.com>
2017-04-26 16:56:02 -0500

    Add option to override the config

--- src/Providers/LaravelPaymeAlignetServiceProvider.php
+++ src/Providers/LaravelPaymeAlignetServiceProvider.php
@@ -9,13 +9,17 @@ class LaravelPaymeAlignetServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->publishes([
-            __DIR__ . '/../config/laravel-payme.php' => config_path('laravel-payme.php'),
-        ], 'laravel.payme.config');
+        if (function_exists('config_path')) {
+            $this->publishes([
+                __DIR__ . '/../config/laravel-payme.php' => config_path('laravel-payme.php'),
+            ], 'laravel.payme.config');
+        }
     }
 
     public function register()
     {
+        $this->mergeConfigFrom(__DIR__ . '/../config/laravel-payme.php', 'laravel-payme');
+
         $this->app->bind('laravel.payme.alignet', LaravelPayme::class);
     }
 }
